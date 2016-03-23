63dfd19e3ba302d3f92eb67e23e903e1abfc94e5
Wildog <i@wil.dog>
2016-03-23 19:49:45 +0800

    update

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -45,23 +45,23 @@ flags = [
 '-DNDEBUG',
 # You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
 # source code needs it.
-'-DUSE_CLANG_COMPLETER',
+#'-DUSE_CLANG_COMPLETER',
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
 # language to use when compiling headers. So it will guess. Badly. So C++
 # headers will be compiled as C headers. You don't want that so ALWAYS specify
 # a "-std=<something>".
 # For a C project, you would set this to something like 'c99' instead of
 # 'c++11'.
-'-std=c++11',
-# -std=c99',
+# '-std=c++11',
+'-std=c99',
 # -stdlib=libc++,
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
-'c++',
-# 'c',
+# 'c++',
+'c',
 '-isystem',
 '../BoostParts',
 '-isystem',
